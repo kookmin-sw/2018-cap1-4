d8fa88ff0a9f76504400a59a4dc129f87a5044f2
wonyong <zcx5674@gmail.com>
2018-03-18 21:45:30 +0900

    add/ dto_memberVO

---
+++ src/main/java/com/company/dto/MemberVO.java
@@ -0,0 +1,46 @@
+package com.company.dto;
+/**
+ * 평가 테이블을 관리하기 위한 DTO
+ * 
+ *
+ */
+public class MemberVO {
+
+	private String patientNum; // 환자 번호
+	private String patientName; // 환자 이름
+	private String comment;
+	private String date;
+	private int score;
+	public String getPatientNum() {
+		return patientNum;
+	}
+	public void setPatientNum(String patientNum) {
+		this.patientNum = patientNum;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
