5c399e9d782bfaf10b41e8a04d684982b317f006
chacha <jjcec@naver.com>
2018-05-28 11:20:59 +0900

    modify resultPage UI

--- src/main/java/com/company/controller/SurveyController.java
+++ src/main/java/com/company/controller/SurveyController.java
@@ -68,8 +68,8 @@ public class SurveyController {
 	{
 		logger.info("resultPage");
 		
-		GeneralSurveyVO resultVO = ruleService.getTypeRule(typeNum);
-		model.addAttribute("resultVO", resultVO);
+		//GeneralSurveyVO resultVO = ruleService.getTypeRule(typeNum);
+		//model.addAttribute("resultVO", resultVO);
 		return "resultPage";
 	}
 	/**
